51a1d6a9ef7ade00b224846226512ef6ec49099a
Muhammad Halif <mhalifm08@gmail.com>
2023-09-09 11:34:50 +0700

    Add unit test and code coverage

--- blog/tests.py
+++ blog/tests.py
@@ -1,3 +1,12 @@
 from django.test import TestCase
+from .models import Post
 
 # Create your tests here.
+class ModelTesting(TestCase):
+    def setUp(self):
+        self.post = Post.objects.create(title='test post', author='test', message='test message')
+    
+    def test_post_model(self):
+        post = self.post
+        self.assertTrue(isinstance(post, Post))
+        self.assertEqual(str(post), 'test post')

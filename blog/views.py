72040268598ea98328e4af6f6525a64a2c0fb45e
Muhammad Halif <mhalifm08@gmail.com>
2023-09-09 17:51:40 +0700

    Add vercel deployment setup

--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,26 @@
 from django.shortcuts import render
 
+from .models import Post
+from .forms import PostForm
+from django.http import HttpResponseRedirect
+
+
+
 # Create your views here.
+def index(request):
+    posts = Post.objects.all()
+    response = {'posts': posts}
+    return render(request, 'index.html', response)
+
+def add_post(request):
+    failed = False
+    if request.method == 'POST':
+        form = PostForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return HttpResponseRedirect('/blog')
+        else:
+            failed = True
+    
+    form = PostForm()
+    return render(request, 'add.html', {'form':form, 'failed':failed})
